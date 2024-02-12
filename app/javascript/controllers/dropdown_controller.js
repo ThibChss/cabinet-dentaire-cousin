83b1d815cdfd758e03e978bb158b9d4462bcdbaf
Thibault Chassine <thibault.chassine@gmail.com>
2024-02-12 13:35:43 +0100

    added click outside burger

--- app/javascript/controllers/dropdown_controller.js
+++ app/javascript/controllers/dropdown_controller.js
@@ -1,15 +1,24 @@
 import { Controller } from "@hotwired/stimulus"
+import { useClickOutside } from 'stimulus-use'
 
 // Connects to data-controller="dropdown"
 export default class extends Controller {
 
   static targets = [
-    'menu'
+    'menu',
+    'burger'
   ]
   connect() {
+    useClickOutside(this)
   }
 
-  toggleDropdown(event) {
+  clickOutside(event) {
+    event.preventDefault()
+    this.menuTarget.classList.remove('active')
+    this.burgerTarget.checked = false
+  }
+
+  toggleDropdown() {
     this.menuTarget.classList.toggle('active')
   }
 }
