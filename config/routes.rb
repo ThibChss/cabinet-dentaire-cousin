29ef926a2285430e0caa0e0ddd87358a7a3d89ff
Thibault Chassine <thibault.chassine@gmail.com>
2024-02-12 14:21:42 +0100

    added page cabinet

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root to: 'pages#construction'
 
-  get :home, to: 'pages#home'
+  get :accueil, to: 'pages#home'
   get :equipe, to: 'pages#team'
   get :documentation, to: 'pages#documentation'
+  get :cabinet, to: 'pages#office'
 end
