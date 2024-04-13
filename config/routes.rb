7b5bf891663727f50a4d3f3366b23f11eecade11
Thibault <Chassine>
2024-04-13 19:47:29 +0200

    finished team page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # root to: 'pages#construction'
-  root to: 'pages#home', as: :accueil
+  root to: 'pages#home'
 
   # get :accueil, to: 'pages#home'
   get :equipe, to: 'pages#team'
