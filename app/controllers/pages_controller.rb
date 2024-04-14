49aab2e463b228c13f2271ef02190b00b18d09e5
Thibault <Chassine>
2024-04-14 12:55:13 +0200

    fix display and add link to footer

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,15 @@
+require "browser/aliases"
+
 class PagesController < ApplicationController
+  Browser::Base.include(Browser::Aliases)
+
   def construction; end
 
   def home
     @browser = Browser.new(request.user_agent)
 
-    @browser = @browser.chrome? || @browser.safari? || @browser.edge?
+    @check_browser = @browser.chrome? || @browser.safari? || @browser.edge?
+    @check_mobile = @browser.mobile?
   end
 
   def team; end
